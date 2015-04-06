8fef1edd153ac36d38838dfbbab3441466bd347a
Tim Sheerman-Chase <orders2008@sheerman-chase.org.uk>
2015-04-06 12:49:33 +0100

    Return nodes for web query

--- osmapi.py
+++ osmapi.py
@@ -20,14 +20,32 @@ class ApiMap(object):
 	def Render(self):
 		webInput = web.input()
 		ziggDb = web.ctx.ziggDb
+		idCount = {"node": 1, "way": 1, "relation": 1}
 
 		bbox = map(float, webInput["bbox"].split(","))
 		area = ziggDb.GetArea(bbox)
 
-		
+		out = [u"<?xml version='1.0' encoding='UTF-8'?>\n"]
+		out.append(u"<osm version='0.6' upload='true' generator='ZiggDb'>\n")
+  		out.append(u"<bounds minlat='{0}' minlon='{1}' maxlat='{2}' maxlon='{3}' origin='ZiggDb' />".format(
+			bbox[1], bbox[0], bbox[3], bbox[2]))
+
+		for nodeId in area["nodes"]:
+			objShapes, objData = area["nodes"][nodeId]
+			shape = objShapes[0]
+			outer, inners = shape
+			pt = outer[0]
+
+			out.append(u"<node id='{0}' timestamp='2006-11-30T00:03:33Z' uid='1' user='ZiggDb' visible='true' version='1' changeset='1' lat='{1}' lon='{2}'>\n".format(idCount["node"], pt[0], pt[1]))
+			idCount["node"] += 1
+			for key in objData:
+				out.append(u"  <tag k='{0}' v='{1}' />\n".format(escape(key), escape(objData[key])))
+			out.append(u"</node>\n")
+
+		out.append(u"</osm>\n")
 
 		web.header('Content-Type', 'text/xml')
-		return "<osm></osm>"
+		return "".join(out).encode("utf-8")
 
 urls = (
 	'/api/0.6/map', 'ApiMap',
