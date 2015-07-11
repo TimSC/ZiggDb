6539fe7d2df1b453a08a9379a48c8067c1006373
Tim <tim2009@sheerman-chase.org.uk>
2015-07-11 22:24:17 +0100

    Check data has been uploaded

--- osmtest/multiobjedit.py
+++ osmtest/multiobjedit.py
@@ -15,6 +15,28 @@ def InterpretUploadResponse(response):
 		diff[el.tag][int(el.attrib["old_id"])] = val
 	return diff
 
+def InterpretDownloadedArea(downloadedData):
+	root = ET.fromstring(downloadedData)
+	if root.tag == "html":
+		raise RuntimeError("Unexpected html in response")
+	data = {"node": {}, "way": {}, "relation": {}}
+	for el in root:
+		objTy = el.tag
+		if objTy == "bounds": 
+			continue
+		data[objTy][int(el.attrib["id"])] = el		
+	return data
+
+def CheckWayHasChildNodes(wayXml, nodeIds):
+	tmp = set()
+	for el in wayXml:
+		if el.tag != "nd": continue
+		tmp.add(int(el.attrib["ref"]))
+	for nid in nodeIds:
+		if int(nid) not in tmp:
+			return False
+	return True
+
 def TestMultiObjectEditing(userpass, verbose=0, save=False):
 
 	log = open("log.txt", "wt")
@@ -34,16 +56,16 @@ def TestMultiObjectEditing(userpass, verbose=0, save=False):
 	cid = int(response[0])
 	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error creating changeset")
 
-	lat = 51.25022331526812
-	lon = -0.6042092878597837
+	lat = [51.25022331526812, 51.2419166618214]
+	lon = [-0.6042092878597837, -0.5910182209303836]
 
 	if verbose>=1: print "Create way between two nodes"
 	#Create a way between two nodes
 	create = "<?xml version='1.0' encoding='UTF-8'?>\n" +\
 	"<osmChange version='0.6' generator='JOSM'>\n" +\
 	"<create version='0.6' generator='JOSM'>\n" +\
-	"  <node id='-289' changeset='"+str(cid)+"' lat='"+str(lat)+"' lon='"+str(lon)+"' />\n" +\
-	"  <node id='-2008' changeset='"+str(cid)+"' lat='51.2419166618214' lon='-0.5910182209303836' />\n"+\
+	"  <node id='-289' changeset='{0}' lat='{1}' lon='{2}' />\n".format(cid, lat[0], lon[0]) +\
+	"  <node id='-2008' changeset='{0}' lat='{1}' lon='{2}' />\n".format(cid, lat[1], lon[1])+\
 	"  <way id='-2010' changeset='"+str(cid)+"'>\n"+\
 	"    <nd ref='-289' />\n"+\
 	"    <nd ref='-2008' />\n"+\
@@ -59,9 +81,9 @@ def TestMultiObjectEditing(userpass, verbose=0, save=False):
 	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error creating node")
 	
 	diff = InterpretUploadResponse(response[0])
-	nodeId1 = diff["node"][-289]["new_id"]
-	nodeId2 = diff["node"][-2008]["new_id"]
-	wayId = diff["way"][-2010]["new_id"]
+	nodeId1 = int(diff["node"][-289]["new_id"])
+	nodeId2 = int(diff["node"][-2008]["new_id"])
+	wayId = int(diff["way"][-2010]["new_id"])
 
 	if verbose>=1: print "Close changeset"
 	#Close the changeset
@@ -69,11 +91,17 @@ def TestMultiObjectEditing(userpass, verbose=0, save=False):
 	if verbose>=2: print response
 	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error closing changeset")
 
-	if 0:
-		#Read back node
-		response = Get(conf.baseurl+"/0.6/node/"+str(nodeId1))
-		if verbose>=2: print response
-		if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error reading back node")
+	#Read back area containing node
+	bbox = [min(lon), min(lat), max(lon), max(lat)]
+	response = Get(conf.baseurl+"/0.6/map?bbox={0}".format(",".join(map(str, bbox))))
+	if verbose>=2: print response
+	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error reading back area")
+	data = InterpretDownloadedArea(response[0])
+	node1Readback = data["node"][nodeId1]
+	node2Readback = data["node"][nodeId2]
+	wayReadback = data["way"][wayId]
+	if not CheckWayHasChildNodes(wayReadback, [nodeId1, nodeId2]):
+		return (0,"Error way has incorrect child nodes")
 
 	if verbose>=1: print "Open changeset"
 	#Open another changeset
