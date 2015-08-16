33252d92a933d715f32021bbea510cea71c2b993
Tim <tim2009@sheerman-chase.org.uk>
2015-08-16 20:50:53 +0100

    New relation test fails

--- osmtest/multiobjedit.py
+++ osmtest/multiobjedit.py
@@ -405,46 +405,46 @@ def TestMultiObjectEditing(userpass, verbose=0, save=False):
 
 	#************ Area operations *******************
 
-	if verbose>=1: print "Open changeset"
-	#Create a changeset
-	createChangeset = "<?xml version='1.0' encoding='UTF-8'?>\n" +\
-	"<osm version='0.6' generator='JOSM'>\n" +\
-	"  <changeset  id='0' open='false'>\n" +\
-	"    <tag k='comment' v='python test function' />\n" +\
-	"    <tag k='created_by' v='JOSM/1.5 (3592 en_GB)' />\n" +\
-	"  </changeset>\n" +\
-	"</osm>\n"
-
-	response = Put(conf.baseurl+"/0.6/changeset/create",createChangeset,userpass)
-	if verbose>=2: print response
-	cid = int(response[0])
-	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error creating changeset")
-
-	lat = [51.22203966503958, 51.219443198163034, 51.22001505165771]
-	lon = [0.41224712359411547, 0.413727688103378, 0.4109146155357791]
-
-	if verbose>=1: print "Create an initial node"
-	#Create a way between two nodes
-	create = "<?xml version='1.0' encoding='UTF-8'?>\n" +\
-	"<osmChange version='0.6' generator='JOSM'>\n" +\
-	"<create version='0.6' generator='JOSM'>\n" +\
-	"  <node id='-289' changeset='{0}' lat='{1}' lon='{2}' />\n".format(cid, lat[0], lon[0]) +\
-	"</create>\n" +\
-	"</osmChange>\n"
-	response = Post(conf.baseurl+"/0.6/changeset/"+str(cid)+"/upload",create,userpass)
-	if verbose>=2: print response
-	if save: open("add.html", "wt").write(response[0])
-	if log is not None: 
-		log.write(response[1])
-		log.write(response[0])
-	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error creating node")
-	
-	diff = InterpretUploadResponse(response[0])
-	nodeId1 = int(diff["node"][-289]["new_id"])
-
 	tagGroupsToTest = [{"natural": "water"}, {"highway": "primary"}, {}]
-
 	for tagGroupToTest in tagGroupsToTest:
+
+		if verbose>=1: print "Open changeset"
+		#Create a changeset
+		createChangeset = "<?xml version='1.0' encoding='UTF-8'?>\n" +\
+		"<osm version='0.6' generator='JOSM'>\n" +\
+		"  <changeset  id='0' open='false'>\n" +\
+		"    <tag k='comment' v='python test function' />\n" +\
+		"    <tag k='created_by' v='JOSM/1.5 (3592 en_GB)' />\n" +\
+		"  </changeset>\n" +\
+		"</osm>\n"
+
+		response = Put(conf.baseurl+"/0.6/changeset/create",createChangeset,userpass)
+		if verbose>=2: print response
+		cid = int(response[0])
+		if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error creating changeset")
+
+		lat = [51.22203966503958, 51.219443198163034, 51.22001505165771]
+		lon = [0.41224712359411547, 0.413727688103378, 0.4109146155357791]
+
+		if verbose>=1: print "Create an initial node"
+		#Create a way between two nodes
+		create = "<?xml version='1.0' encoding='UTF-8'?>\n" +\
+		"<osmChange version='0.6' generator='JOSM'>\n" +\
+		"<create version='0.6' generator='JOSM'>\n" +\
+		"  <node id='-289' changeset='{0}' lat='{1}' lon='{2}' />\n".format(cid, lat[0], lon[0]) +\
+		"</create>\n" +\
+		"</osmChange>\n"
+		response = Post(conf.baseurl+"/0.6/changeset/"+str(cid)+"/upload",create,userpass)
+		if verbose>=2: print response
+		if save: open("add.html", "wt").write(response[0])
+		if log is not None: 
+			log.write(response[1])
+			log.write(response[0])
+		if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error creating node")
+	
+		diff = InterpretUploadResponse(response[0])
+		nodeId1 = int(diff["node"][-289]["new_id"])
+
 		if verbose>=1: print "Create an area between three nodes", tagGroupToTest
 		#Create a way between two nodes
 		create = "<?xml version='1.0' encoding='UTF-8'?>\n" +\
@@ -500,7 +500,6 @@ def TestMultiObjectEditing(userpass, verbose=0, save=False):
 		node3Readback = data["node"][nodeId3]
 		if not CheckNodePosition(node3Readback, lat[2], lon[2]):
 			return (0,"Error node has bad position")
-		#Result is a relation of a different ID?
 		wayReadback = data["way"][wayId]
 		if not CheckWayHasChildNodes(wayReadback, [nodeId1, nodeId2, nodeId3]):
 			return (0,"Error way has incorrect child nodes")
@@ -522,6 +521,101 @@ def TestMultiObjectEditing(userpass, verbose=0, save=False):
 		if len(response[0]) > 0: print response[0]
 		if save: open("verify.html", "wt").write(response[0])
 
+
+	#****** Relation operations *************
+
+	if verbose>=1: print "Open changeset"
+	#Create a changeset
+	createChangeset = "<?xml version='1.0' encoding='UTF-8'?>\n" +\
+	"<osm version='0.6' generator='JOSM'>\n" +\
+	"  <changeset  id='0' open='false'>\n" +\
+	"    <tag k='comment' v='python test function' />\n" +\
+	"    <tag k='created_by' v='JOSM/1.5 (3592 en_GB)' />\n" +\
+	"  </changeset>\n" +\
+	"</osm>\n"
+
+	response = Put(conf.baseurl+"/0.6/changeset/create",createChangeset,userpass)
+	if verbose>=2: print response
+	cid = int(response[0])
+	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error creating changeset")
+
+	lat = [51.22203966503958, 51.219443198163034, 51.22001505165771]
+	lon = [0.41224712359411547, 0.413727688103378, 0.4109146155357791]
+
+	if verbose>=1: print "Create an area using relations"
+	#Create a way between two nodes
+	tagGroupToTest = {"natural": "water", "type": "multipolygon"}
+	create = "<?xml version='1.0' encoding='UTF-8'?>\n" +\
+	"<osmChange version='0.6' generator='JOSM'>\n" +\
+	"<create>\n"+\
+	"  <node id='-359' changeset='{0}' lat='51.21420467163' lon='-0.1701214362' />\n".format(cid)+\
+	"  <node id='-357' changeset='{0}' lat='51.21664687637' lon='-0.18072625879' />\n".format(cid)+\
+	"  <node id='-371' changeset='{0}' lat='51.21669571914' lon='-0.17307116972' />\n".format(cid)+\
+	"  <node id='-370' changeset='{0}' lat='51.21840518357' lon='-0.17010805752' />\n".format(cid)+\
+	"  <node id='-373' changeset='{0}' lat='51.21879590938' lon='-0.17517653891' />\n".format(cid)+\
+	"  <node id='-367' changeset='{0}' lat='51.21962619072' lon='-0.1696401977' />\n".format(cid)+\
+	"  <node id='-361' changeset='{0}' lat='51.22026110165' lon='-0.16357139872' />\n".format(cid)+\
+	"  <node id='-365' changeset='{0}' lat='51.22162857236' lon='-0.17338307627' />\n".format(cid)+\
+	"  <node id='-364' changeset='{0}' lat='51.22319134628' lon='-0.17096580053' />\n".format(cid)+\
+	"  <node id='-356' changeset='{0}' lat='51.22680505777' lon='-0.17230478203' />\n".format(cid)+\
+	"  <way id='-366' changeset='{0}'>\n".format(cid)+\
+	"	<nd ref='-364' />\n"+\
+	"	<nd ref='-365' />\n"+\
+	"	<nd ref='-367' />\n"+\
+	"	<nd ref='-364' />\n"+\
+	"  </way>\n"+\
+	"  <way id='-372' changeset='{0}'>\n".format(cid)+\
+	"	<nd ref='-370' />\n"+\
+	"	<nd ref='-371' />\n"+\
+	"	<nd ref='-373' />\n"+\
+	"	<nd ref='-370' />\n"+\
+	"  </way>\n"+\
+	"  <way id='-358' changeset='{0}'>\n".format(cid)+\
+	"	<nd ref='-356' />\n"+\
+	"	<nd ref='-357' />\n"+\
+	"	<nd ref='-359' />\n"+\
+	"	<nd ref='-361' />\n"+\
+	"	<nd ref='-356' />\n"+\
+	"  </way>\n"+\
+	"  <relation id='-435' changeset='{0}'>\n".format(cid)+\
+	"	<member type='way' ref='-366' role='outer' />\n"+\
+	"	<member type='way' ref='-372' role='inner' />\n"+\
+	"	<member type='way' ref='-358' role='inner' />\n"
+	for key in tagGroupToTest:
+		create += "    <tag k='{0}' v='{1}' />\n".format(quoteattr(key), quoteattr(tagGroupToTest[key]))
+	create += "  </relation>\n"+\
+	"</create>\n"+\
+	"</osmChange>\n"
+	response = Post(conf.baseurl+"/0.6/changeset/"+str(cid)+"/upload",create,userpass)
+	if verbose>=2: print response
+	if save: open("add.html", "wt").write(response[0])
+	if log is not None: 
+		log.write(response[1])
+		log.write(response[0])
+	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error creating area")
+
+	diff = InterpretUploadResponse(response[0])
+	wayId1 = int(diff["way"][-366]["new_id"])
+	wayId2 = int(diff["way"][-372]["new_id"])
+	wayId3 = int(diff["way"][-358]["new_id"])
+	relId = int(diff["relation"][-435]["new_id"])
+
+	if verbose>=1: print "Close changeset"
+	#Close the changeset
+	response = Put(conf.baseurl+"/0.6/changeset/"+str(cid)+"/close","",userpass)
+	if verbose>=2: print response
+	if HeaderResponseCode(response[1]) != "HTTP/1.1 200 OK": return (0,"Error closing changeset")
+
+	#Verify cache in this area
+	bbox = [min(lon), min(lat), max(lon), max(lat)]
+	response = Get(conf.baseurl+"/0.6/verifycache?bbox={0}".format(",".join(map(str, bbox))))
+	if len(response[0]) > 0: print response[0]
+
+	#Verify underlying database integrity in this area
+	bbox = [min(lon), min(lat), max(lon), max(lat)]
+	response = Get(conf.baseurl+"/0.6/verifydb?bbox={0}".format(",".join(map(str, bbox))))
+	if len(response[0]) > 0: print response[0]
+	if save: open("verify.html", "wt").write(response[0])
 
 	return (1,"OK")
 
